3c65dcd912b51ea501ff9c0cd4af0a46edb6cdc0
Kamil Zięba <80387021+MagicznySmalec@users.noreply.github.com>
2021-11-07 19:19:28 +0100

    Player Movement

--- Frst Game/Assets/Scripts/Mover.cs
+++ Frst Game/Assets/Scripts/Mover.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Mover : MonoBehaviour
+
+
 {
+    Rigidbody rb;
+
+    [SerializeField] float moveSpeed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float xValue = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime; 
+
+        float zValue = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        transform.Translate(xValue, 0, zValue);
     }
 }
