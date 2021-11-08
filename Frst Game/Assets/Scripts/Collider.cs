77e70105ab5e4e920dd85c3be1f8ee6483fa6a4d
Kamil Zięba <80387021+MagicznySmalec@users.noreply.github.com>
2021-11-08 13:31:33 +0100

    Version 0.0.2
    Added Sound Effects and level progression

--- Frst Game/Assets/Scripts/Collider.cs
+++ Frst Game/Assets/Scripts/Collider.cs
@@ -1,9 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class Collider : MonoBehaviour
 {
+    [SerializeField] float reloadDelay = 1f;
+    [SerializeField] float nextLvlDelay = 1f;
+    [SerializeField] AudioClip trumpet;
+    [SerializeField] AudioClip coin;
+
+    Rigidbody rb;
+    AudioSource auso;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        auso = GetComponent<AudioSource>();
+    }
+
     private void OnCollisionEnter(Collision other)
     {
         switch (other.gameObject.tag)
@@ -13,14 +26,61 @@ public class Collider : MonoBehaviour
                     break;
             case "Enemy":
                     Debug.Log("Enemy has killed you!");
+                    Death();
+                    Invoke("levelReload", reloadDelay);
                     break;
             case "Collectible":
                     Debug.Log("Congrats !");
+                    CoinSound();
+                    CoinGrabbed();
+                    Invoke("NextLevelLoad", nextLvlDelay);
                     break;
             default:
                     Debug.Log("You crashed into an obstacle");
+                    Death();
+                    Invoke("levelReload", reloadDelay);
                     break;
         }
     }   
 
+    void Death()
+    {
+        GetComponent<Mover>().enabled = false;
+    }
+
+
+    void CoinGrabbed()
+    {
+        GetComponent<Mover>().enabled = false;
+        if (!auso.isPlaying)
+        {
+            auso.PlayOneShot(trumpet);
+        }
+    }
+
+    void levelReload()
+    {
+        int CurrentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(CurrentSceneIndex);
+    }
+
+    void NextLevelLoad()
+    {
+        int CurrentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = CurrentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    void CoinSound()
+    {
+        if (!auso.isPlaying)
+        {
+            auso.PlayOneShot(coin);
+        }
+    }
 }
+
